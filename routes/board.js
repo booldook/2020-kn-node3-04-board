7fe117d94a87af447a0fb73f41c92fcf560e1d3d
iremys <iremys@gmail.com>
2020-05-10 13:39:50 +0900

    pug 작업중

--- routes/board.js
+++ routes/board.js
@@ -3,4 +3,14 @@ const path = require('path');
 const router = express.Router();
 const { pool } = require('../modules/mysql-conn');
 
+router.get('/list', (req, res, next) => {
+	const pugVals = {cssFile: "board", jsFile: "board"};
+	res.render("board/list.pug", pugVals);
+})
+
+router.get('/write', (req, res, next) => {
+	const pugVals = {cssFile: "board", jsFile: "board"};
+	res.render("board/write.pug", pugVals);
+})
+
 module.exports = router;
