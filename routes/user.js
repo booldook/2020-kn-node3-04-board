dac76d45dc57b9af2eab04141d389c52e19f8a46
iremys <iremys@gmail.com>
2020-06-07 13:57:45 +0900

    login

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const { alert } = require('../modules/util'); 
 const { pool } = require('../modules/mysql-conn');
 const pugVals = {cssFile: "user", jsFile: "user"};
 
@@ -28,6 +29,33 @@ router.post('/save', async (req, res, next) => {
 		console.log(result);
 		connect.release();
 		res.redirect("/");
+	}
+	catch(e) {
+		connect.release();
+		next(e);
+	}
+});
+
+router.post('/auth', async (req, res, next) => {
+	let {userid, userpw} = req.body;
+	let sql, connect, result;
+	try {
+		if(userid && userpw) {
+			connect = await pool.getConnection();
+			sql = "SELECT userpw FROM user WHERE userid=?";
+			result = await connect.query(sql, [userid]);
+			if(result[0][0]) {
+				result = await bcrypt.compare(userpw + process.env.PASS_SALT, result[0][0].userpw);
+				connect.release();
+				if(result) res.send(alert("회원입니다. 반갑습니다.", '/'));
+				else res.send(alert('아이디와 패스워드를 확인하세요.', '/'));
+			}
+			else {
+				connect.release();
+				res.send(alert('아이디와 패스워드를 확인하세요.', '/'));
+			}
+		}
+		else res.send(alert('아이디와 패스워드를 확인하세요.', '/'));
 	}
 	catch(e) {
 		connect.release();
